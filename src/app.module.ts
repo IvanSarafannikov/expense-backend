3d852cfe107ac86f0368bb9125769b5c0664b382
Vladyslav Pron <vladyslav.pron@incode-it.com>
2022-08-08 16:36:56 +0300

    feat(database): :sparkles: Database integration
    Implemented integration with PostgreSQL database using TypeORM mapper

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env?.['DATABASE_URL'],
+      entities: [],
+      synchronize: true,
+      ssl: true,
+      extra: {
+        ssl: { rejectUnauthorized: false },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
