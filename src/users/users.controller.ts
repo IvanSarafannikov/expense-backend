3e26fd51fbe4ce0bd23f3caf9f51c23a5b028be3
Vladyslav Pron <vladyslav.pron@incode-it.com>
2022-08-09 11:19:35 +0300

    feat(module): :sparkles: users module CRUD endpoints with related service functions
    Created CRUD endpoints for `users` module with corresponding service functions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import type { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -8,5 +17,25 @@ export class UsersController {
   @Get()
   getUsers() {
     return this.usersService.getAllUsers();
+  }
+
+  @Get(':userId')
+  getUser(@Param('userId') userId: number) {
+    return this.usersService.getUserById(userId);
+  }
+
+  @Post()
+  createUser(@Body() user: User) {
+    return this.usersService.createUser(user);
+  }
+
+  @Patch(':userId')
+  updateUser(@Param('userId') userId: number, @Body() userDataToUpdate: User) {
+    return this.usersService.updateUser(userId, userDataToUpdate);
+  }
+
+  @Delete(':userId')
+  deleteUser(@Param('userId') userId: number) {
+    return this.usersService.deleteUserById(userId);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import type { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -11,5 +11,50 @@ export class UsersService {
 
   async getAllUsers() {
     return this.usersRepository.find();
+  }
+
+  async getUserById(id: number) {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+
+  async getUserByUsername(username: string) {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+
+  async createUser(userData: User) {
+    // TODO: implement create-user dto on which create user for better control
+    // TODO: create related entities
+    const user = this.usersRepository.create(userData);
+    await this.usersRepository.save(user);
+    return user;
+  }
+
+  async updateUser(id: number, userDataToUpdate: User) {
+    // TODO: create update-user dto and update entity with it to prevent updating unwanted fields and validation
+
+    const user = await this.usersRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new HttpException(
+        'User you want to update does not exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return this.usersRepository.save({ ...user, ...userDataToUpdate });
+  }
+
+  async deleteUserById(id: number) {
+    // TODO: delete related entities
+    const result = await this.usersRepository.delete({ id });
+
+    if (!result.affected) {
+      throw new HttpException(
+        'User you want to delete does not exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return null;
   }
 }
