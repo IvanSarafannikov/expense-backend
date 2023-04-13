0387bf717d65f2ca7dd450cbe8434aa311acb582
volesh <volesh2@gmail.com>
2023-04-13 13:45:49 +0300

    add .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Expense')
+    .setDescription('The expense-beackend API description')
+    .setVersion('1.0')
+    .addTag('expense')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
